caaadd1d6b287eabc1ae2d46efa3995e2ba5c795
Joseph <joekimga@gmail.com>
2017-09-23 20:05:35 -0400

    WIP: initialize firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+
+
+// Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyAmzFi9GKoB7V6APAZr3goNsZnRGlaDCXA",
+  authDomain: "maidenvoyage-9fa43.firebaseapp.com",
+  databaseURL: "https://maidenvoyage-9fa43.firebaseio.com",
+  projectId: "maidenvoyage-9fa43",
+  storageBucket: "maidenvoyage-9fa43.appspot.com",
+  messagingSenderId: "795370132163"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//button for adding train name
